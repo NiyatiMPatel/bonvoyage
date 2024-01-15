e78d5b938bfdbeffd9a0d4c368063a26045c3a0a
Niyati M Patel <patelnitie@gmail.com>
2024-01-14 21:43:20 -0500

    updated catch all

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -86,5 +86,7 @@ export const logoutController = async (req: Request, res: Response) => {
 
 // CATCH ALL ROUTES CONTROLLER
 export const catchAllController = async (req: Request, res: Response) => {
-  res.send(path.join(__dirname, "..", "..", "frontend", "dist", "index.html"));
+  res.sendFile(
+    path.join(__dirname, "..", "..", "frontend", "dist", "index.html")
+  );
 };
